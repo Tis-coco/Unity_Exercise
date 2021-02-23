58833be4f98d0a45c752ee0d6704af05d29d9f41
Tis-coco <10510226@gm.nttu.edu.tw>
2021-02-23 13:53:21 +0800

    新增旋轉程式碼和6張桌子(一樣的)

--- Github_Exercise/Assets/Developer_1/TableEvent.cs
+++ Github_Exercise/Assets/Developer_1/TableEvent.cs
@@ -7,12 +7,13 @@ public class TableEvent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("RotationTable", 0,0.05f);
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+    }
+    private void RotationTable() {
+        gameObject.GetComponent<Transform>().transform.Rotate(new Vector3(0,0,2));
     }
 }
